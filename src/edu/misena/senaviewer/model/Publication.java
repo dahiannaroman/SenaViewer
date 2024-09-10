69a46a4d338d5af82c3a62f5a64dea39df0c75f4
dahianna roman <karendahiannaromanbarrios@gmail.com>
2024-09-10 18:33:19 -0500

    clase publication

---
+++ src/edu/misena/senaviewer/model/Publication.java
@@ -0,0 +1,52 @@
+package edu.misena.senaviewer.model;
+import java.util.List;
+
+public class Publication {
+    private String title;
+    private String edititionDate;
+    private String editorial;
+    private List<String> authors;
+
+    //constructor
+    public Publication(String title, String edititionDate, String editorial){
+        this.title = title;
+        this.edititionDate = edititionDate;
+        this.editorial = editorial;
+        this.authors = null; //puede ser una lista vacía si no hay autores
+    }
+
+    //getters y setters
+    public String getTitle(){
+        return title;
+    }
+    public void setTitle(String title){
+        this.title = title;
+    }
+    public String getEdititionDate(){
+        return edititionDate;
+    }
+    public void setEdititionDate(String edititioDate){
+        this.edititionDate = edititionDate;
+    }
+    public String getEditorial(){
+        return editorial;
+    }
+    public void setEditorial(String editorial){
+        this.editorial = editorial;
+    }
+    public List<String> getAuthors(){
+        return authors;
+    }
+    public void setAuthors(List<String> authors){
+        this.authors = authors;
+    }
+    @Override
+    public String toString() {
+        return "Publication{" +
+                "title='" + title + '\'' +
+                ", edititionDate='" + edititionDate + '\'' +
+                ", editorial='" + editorial + '\'' +
+                ", authors=" + authors +
+                '}';
+    }
+}
