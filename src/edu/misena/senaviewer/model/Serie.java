06cd3f58c31c1cda415f3e1e1c74ee8af6a9a626
dahianna roman <karendahiannaromanbarrios@gmail.com>
2024-09-12 07:33:12 -0500

    clase serie extencion film

--- src/edu/misena/senaviewer/model/Serie.java
+++ src/edu/misena/senaviewer/model/Serie.java
@@ -1,41 +1,25 @@
 package edu.misena.senaviewer.model;
 
-public class Serie {
-    private int id;
-    private String title;
-    private String genre;
-    private String creator;
-    private int duration;
-    private int year;
-    private boolean viewed;
-    private int timeViewed;
+public class Serie extends Film{
     private int sessionQuantity;
 
-    public Serie(String title, String genre, int duration) {
-        this.title = title;
-        this.genre = genre;
-        this.duration = duration;
+    //constructor
+    public Serie(String title, String genre, int duration, int sessionQuantity){
+        super(title, genre, null, duration);
+        this.sessionQuantity = sessionQuantity;
     }
 
-    // Getters and Setters
-    public int getId() { return id; }
-    public void setId(int id) { this.id = id; }
-    public String getTitle() { return title; }
-    public void setTitle(String title) { this.title = title; }
-    public String getGenre() { return genre; }
-    public void setGenre(String genre) { this.genre = genre; }
-    public String getCreator() { return creator; }
-    public void setCreator(String creator) { this.creator = creator; }
-    public int getDuration() { return duration; }
-    public void setDuration(int duration) { this.duration = duration; }
-    public int getYear() { return year; }
-    public void setYear(int year) { this.year = year; }
-    public boolean isViewed() { return viewed; }
-    public void setViewed(boolean viewed) { this.viewed = viewed; }
-    public int getTimeViewed() { return timeViewed; }
-    public void setTimeViewed(int timeViewed) { this.timeViewed = timeViewed; }
-    public int getSessionQuantity() { return sessionQuantity; }
-    public void setSessionQuantity(int sessionQuantity) { this.sessionQuantity = sessionQuantity; }
+    //getter y setter
+    public int getSessionQuantity(){
+        return sessionQuantity;
+    }
+    public void setSessionQuantity(int sessionQuantity){
+        this.sessionQuantity = sessionQuantity;
+    }
+    @Override
+    public String toString() {
+        return "Series{" +
+                "sessionQuantity=" + sessionQuantity +
+                "} " + super.toString();
+    }
 }
-
-
